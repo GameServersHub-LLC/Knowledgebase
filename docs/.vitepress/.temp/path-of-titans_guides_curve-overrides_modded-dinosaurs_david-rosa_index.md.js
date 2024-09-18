d12dd34ef2c77f8d8beba2a92aa209c0da0510b2
That411guy <mharrison747@gmail.com>
2024-09-17 22:16:14 -0400

    up

---
+++ docs/.vitepress/.temp/path-of-titans_guides_curve-overrides_modded-dinosaurs_david-rosa_index.md.js
@@ -0,0 +1,19 @@
+import { ssrRenderAttrs } from "vue/server-renderer";
+import { useSSRContext } from "vue";
+import { _ as _export_sfc } from "./plugin-vue_export-helper.1tPrXgE0.js";
+const __pageData = JSON.parse(`{"title":"David Rosa","description":"Explore GameServersHub's Gaming Wiki for in-depth info on Path of Titans. Find details on gameplay, features, and updates for the ultimate dino MMO adventure!","frontmatter":{"head":[["meta",{"name":"description","content":"Explore GameServersHub's Gaming Wiki for in-depth info on Path of Titans. Find details on gameplay, features, and updates for the ultimate dino MMO adventure!"}],["meta",{"name":"keywords","content":"Path Of Titans"}]],"title":"David Rosa"},"headers":[],"relativePath":"path-of-titans/guides/curve-overrides/modded-dinosaurs/david-rosa/index.md","filePath":"path-of-titans/guides/curve-overrides/modded-dinosaurs/david-rosa/index.md","lastUpdated":1726618007000}`);
+const _sfc_main = { name: "path-of-titans/guides/curve-overrides/modded-dinosaurs/david-rosa/index.md" };
+function _sfc_ssrRender(_ctx, _push, _parent, _attrs, $props, $setup, $data, $options) {
+  _push(`<div${ssrRenderAttrs(_attrs)}><h1 id="path-of-titans-david-rosa" tabindex="-1">Path of Titans David Rosa <a class="header-anchor" href="#path-of-titans-david-rosa" aria-label="Permalink to &quot;Path of Titans David Rosa&quot;">​</a></h1><p>Explore GameServersHub&#39;s Gaming Wiki for in-depth info on Path of Titans. Find details on gameplay, features, and updates for the ultimate dino MMO adventure!</p><p>This your mod? You want your info here? Make a ticket in our <a href="https://discord.gg/gsh" target="_blank" rel="noreferrer">Discord!</a></p><p>Modder&#39;s <a href="#">Discord Link</a></p><h2 id="kaprosuchus" tabindex="-1">Kaprosuchus <a class="header-anchor" href="#kaprosuchus" aria-label="Permalink to &quot;Kaprosuchus&quot;">​</a></h2><p><a href="./path-of-titans-davidkapronew" target="_blank"><img src="https://web-cdn.alderongames.com/files/1081/conversions/DavidRosaKrpo-icon.jpg"></a></p><h2 id="pachyrhinosaurus" tabindex="-1">Pachyrhinosaurus <a class="header-anchor" href="#pachyrhinosaurus" aria-label="Permalink to &quot;Pachyrhinosaurus&quot;">​</a></h2><p><a href="./path-of-titans-davidrosapachyrhino" target="_blank"><img src="https://web-cdn.alderongames.com/files/1252/conversions/Proyecto-nuevo-(24)-icon.jpg"></a></p></div>`);
+}
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("path-of-titans/guides/curve-overrides/modded-dinosaurs/david-rosa/index.md");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["ssrRender", _sfc_ssrRender]]);
+export {
+  __pageData,
+  index as default
+};
